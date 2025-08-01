191e832814a9760974c3613c1b07da46c9e1c64e
mobin.pourabedini <realmobinpourabedini@gmail.com>
2025-08-01 19:16:52 +0330

    frontend compatiblity

--- src/main/java/ir/sharif/louie/models/dto/auth/JwtAuthenticationResponse.java
+++ src/main/java/ir/sharif/louie/models/dto/auth/JwtAuthenticationResponse.java
@@ -1,10 +1,13 @@
 package ir.sharif.louie.models.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class JwtAuthenticationResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType = "Bearer";
 
     public JwtAuthenticationResponse(String accessToken) {
--- src/main/java/ir/sharif/louie/models/dto/painting/SavePaintingResponse.java
+++ src/main/java/ir/sharif/louie/models/dto/painting/SavePaintingResponse.java
@@ -1,11 +1,14 @@
 package ir.sharif.louie.models.dto.painting;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ir.sharif.louie.models.db.Painting;
 import lombok.Data;
 
 @Data
 public class SavePaintingResponse {
     private Long id;
+
+    @JsonProperty("canvas_data")
     private String canvasData;
 
     public SavePaintingResponse(Painting painting) {
